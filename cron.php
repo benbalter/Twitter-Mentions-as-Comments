5629b14fe180855619b5324014067dab9fa54de2
benbalter <benbalter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-05 20:48:56 +0000

    manual cron

---
+++ cron.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File to be run by cron to check for Twitter mentions at a regular interval
+ * @package Twitter Mentions as Comments
+ * @since 0.3-beta
+ */
+
+//grab WP bootstrap
+require_once('../../../wp-load.php');
+define('TMAC_DOING_CRON', TRUE);
+
+//verify that the plugin is activated, otherwise exit
+if ( !function_exists('tmac_mentions_check') )
+	exit();
+	
+tmac_metions_check();
+	
+?>
