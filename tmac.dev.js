fd1f428370e903971c0c1fcbc18afcd065671bee
benbalter <benbalter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-17 15:02:51 +0000

    js

---
+++ tmac.dev.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($){
+
+	if ( pagenow && pagenow == 'settings_page_tmac_options' ) {
+
+		//cron details
+		$('#cron-details').siblings('input').click(function(){ 
+ 			if ( $(this).val() == 1)
+   				$('#cron-details').slideDown();
+   			else 
+   				$('#cron-details').slideUp();
+		});	
+		if ( tmac.hide_manual_cron_details ) {
+    		$('#cron-details').hide();
+    	}
+		
+		//donate button
+		$('#hide-donate').click( function(event){
+			$.ajax({
+				url: 'admin-ajax.php?action=tmac_hide_donate&_ajax_nonce-tmac-hide-donate=' + $('#_ajax_nonce-tmac-hide-donate').val(),
+				success: function() {
+					$('#donate').fadeOut();
+				}
+			});
+			event.preventDefault();
+			return false;
+		});
+	}
+	
+});
+
