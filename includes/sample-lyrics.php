3e2be693cbcc6e5574dee3ad1d930a1d2eda4f17
Benjamin J. Balter <ben@balter.com>
2012-01-29 11:36:07 -0500

    code cleanup

--- includes/sample-lyrics.php
+++ includes/sample-lyrics.php
@@ -1,63 +1,92 @@
 <?php
 /**
  * Main Hello Dolly 2.0 Logic
- * @package Plugin_Boilerplate
  * @subpackage Hello_Dolly2
+ * @author Benjamin J. Balter <ben@balter.com>
+ * @package Plugin_Boilerplate
  */
- 
+
 class Hello_Dolly2_Sample_Lyrics {
-	
+
 	private $parent;
 	public $lyrics;
-	
+
+	/**
+	 * Init Default Lyrics and register with WordPress API
+	 * @param class $parent (reference) the Parent Class
+	 */
 	function __construct( &$parent ) {
-		
+
 		$this->parent = &$parent;
-			
-		$this->lyrics = array ( 
-							__( "Hello, Dolly" ),
-	    					__( "Well, hello ), Dolly" ),
-	    					__( "It's so nice to have you back where you belong" ),
-	    					__( "You're lookin' swell, Dolly" ),
-	    					__( "I can tell, Dolly" ),
-	    					__( "You're still glowin', you're still crowin'" ),
-	    					__( "You're still goin' strong" ),
-	    					__( "We feel the room swayin'" ),
-	    					__( "While the band's playin'" ),
-	    					__( "One of your old favourite songs from way back when" ),
-	    					__( "So, take her wrap, fellas" ),
-	    					__( "Find her an empty lap, fellas" ),
-	    					__( "Dolly'll never go away again" ),
-	    					__( "Hello, Dolly" ),
-	    					__( "Well, hello, Dolly" ),
-	    					__( "It's so nice to have you back where you belong" ),
-	    					__( "You're lookin' swell, Dolly" ),
-	    					__( "I can tell, Dolly" ),
-	    					__( "You're still glowin', you're still crowin'" ),
-	    					__( "You're still goin' strong" ),
-	    					__( "We feel the room swayin'" ),
-	    					__( "While the band's playin'" ),
-	    					__( "One of your old favourite songs from way back when" ),
-	    					__( "Golly, gee, fellas" ),
-	    					__( "Find her a vacant knee, fellas" ),
-	    					__( "Dolly'll never go away" ),
-	    					__( "Dolly'll never go away" ),
-	    					__( "Dolly'll never go away again" ),
-	    			);
-		
+
+		$this->lyrics = array (
+			__( "Hello, Dolly" ),
+			__( "Well, hello, Dolly" ),
+			__( "It's so nice to have you back where you belong" ),
+			__( "You're lookin' swell, Dolly" ),
+			__( "I can tell, Dolly" ),
+			__( "You're still glowin', you're still crowin'" ),
+			__( "You're still goin' strong" ),
+			__( "We feel the room swayin'" ),
+			__( "While the band's playin'" ),
+			__( "One of your old favourite songs from way back when" ),
+			__( "So, take her wrap, fellas" ),
+			__( "Find her an empty lap, fellas" ),
+			__( "Dolly'll never go away again" ),
+			__( "Hello, Dolly" ),
+			__( "Well, hello, Dolly" ),
+			__( "It's so nice to have you back where you belong" ),
+			__( "You're lookin' swell, Dolly" ),
+			__( "I can tell, Dolly" ),
+			__( "You're still glowin', you're still crowin'" ),
+			__( "You're still goin' strong" ),
+			__( "We feel the room swayin'" ),
+			__( "While the band's playin'" ),
+			__( "One of your old favourite songs from way back when" ),
+			__( "Golly, gee, fellas" ),
+			__( "Find her a vacant knee, fellas" ),
+			__( "Dolly'll never go away" ),
+			__( "Dolly'll never go away" ),
+			__( "Dolly'll never go away again" ),
+		);
+
 		add_action( 'hd2_options_init', array( &$this, 'options_init' ) );
 		add_filter( $this->parent->prefix . 'lyric', array( &$this, 'add_p_tag' ) );
 		add_action( 'admin_notices', array( &$this, 'lyric' ) );
 		add_action( 'admin_head', array( &$this, 'css' ) );
 		add_action( 'admin_init', array( &$this, 'js_lyrics' ) );
+		add_action( 'admin_init', array( &$this, 'reset_lyrics' ) );
 	}
-	
+
+
+	/**
+	 * Init Options
+	 */
 	function options_init() {
+
 		$this->parent->options->defaults = array( 'lyrics' => &$this->lyrics );
+
 	}
-	
+
+
+	/**
+	 * Restore lyrics to defaults
+	 */
+	function reset_lyrics() {
+
+		if ( !isset( $_GET[ 'hd2_reset' ] ) || !$_GET[ 'hd2_reset' ] )
+			return;
+
+		$this->parent->options->lyrics = $this->lyrics;
+		wp_redirect( admin_url( 'options-general.php?page=hd2_options&settings-updated=true' ) );
+		exit();
+
+	}
+
+
 	/**
 	 * Returns texturized array of all lyrics
+	 * @return array the lyrics
 	 */
 	function get_lyrics() {
 		$lyrics = $this->parent->options->lyrics;
@@ -65,44 +94,53 @@ class Hello_Dolly2_Sample_Lyrics {
 		return $lyrics;
 	}
 
+
 	/**
 	 * Returns a single lyric
 	 * @uses hd2_lyric filter
+	 * @return string random lyric
 	 */
 	function get_lyric() {
 
 		$lyrics = $this->get_lyrics();
 		$lyric = $lyrics[ array_rand( $lyrics ) ];
 		return $this->parent->api->apply_filters( 'lyric', $lyric );
-		
+
 	}
-	
+
+
 	/**
 	 * Enqueue lyrics via localize script
 	 */
 	function js_lyrics() {
 		$this->parent->enqueue->admin_data['lyrics'] = $this->get_lyrics();
 	}
-	
+
+
 	/**
 	 * Echos a lyric
 	 */
 	function lyric() {
 		echo $this->get_lyric();
 	}
-	
+
+
 	/**
 	 * Filter to wrap lyric in <p> tags
+	 * @param string $lyric the lyrics
+	 * @return string the lytics wrapped in <p> tags
 	 */
-	function add_p_tag( $lyric ) { 
+	function add_p_tag( $lyric ) {
 		return $this->parent->template->get( 'sample-header', compact( 'lyric' ) );
 	}
-	
+
+
 	/**
 	 * Inject CSS into admin header
 	 */
 	function css() {
 		$this->parent->template->load( 'sample-css' );
 	}
-		
+
+
 }
--- sample-plugin.php
+++ sample-plugin.php
@@ -1,53 +1,107 @@
 <?php
 /*
 Plugin Name: Plugin Boilerplate Sample
-Description: 
+Description:
 Version: 1.0
 Author: Benjamin J. Balter
 Author URI: http://ben.balter.com
-License: GPL2
+License: GPL3
 */
 
-/*  Copyright 2012  Benjamin J. Balter  (email : ben@balter.com)
+/*  Copyright 2012  Benjamin J. Balter  ( ben@balter.com | http://ben.balter.com )
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 	@copyright 2011-2012
+ *	@license GPL v3
+ * 	@version 1.0
+ * 	@package Plugin Boilerplate
+ *	@subpackage Hello_Dolly2
+ * 	@author Benjamin J. Balter <ben@balter.com>
+ */
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-require_once( dirname( __FILE__ ) . '/includes/class.plugin-boilerplate.php' );
+require_once dirname( __FILE__ ) . '/includes/class.plugin-boilerplate.php';
 
 class Hello_Dolly2 extends Plugin_Boilerplate {
-	
+
 	static $instance;
-	public $name = 'Hello Dolly 2.0'; //Human-readable name of plugin
-	public $slug = 'hello-dolly2'; //plugin slug, generally base filename and in url on wordpress.org
-	public $slug_ = 'hello_dolly2'; //slug with underscores (PHP/JS safe)
-	public $prefix = 'hd2_'; //prefix to append to all options, API calls, etc. w/ trailing underscore
+	public $name      = 'Hello Dolly 2.0'; //Human-readable name of plugin
+	public $slug      = 'hello-dolly2'; //plugin slug, generally base filename and in url on wordpress.org
+	public $slug_     = 'hello_dolly2'; //slug with underscores (PHP/JS safe)
+	public $prefix    = 'hd2_'; //prefix to append to all options, API calls, etc. w/ trailing underscore
 	public $directory = null;
-	public $version = '1.0';
+	public $version   = '1.0';
 
 	/**
 	 * Construct the boilerplate and autoload all child classes
 	 */
 	function __construct() {
-	
+
 		self::$instance = &$this;
 		$this->directory = dirname( __FILE__ );
 		parent::__construct( &$this );
-		
+
+		add_action( 'admin_menu', array( &$this, 'options_menu_init' ) );
+		add_action( 'admin_init', array( &$this, 'register_setting' ) );
+
 	}
-	
-	
+
+
+	/**
+	 * Register our option with WordPress
+	 */
+	function register_setting() {
+		register_setting( $this->slug_, $this->slug_, array( &$this, 'options_validate' ) );
+	}
+
+
+	/**
+	 * Register our menu with WordPress
+	 */
+	function options_menu_init() {
+		add_options_page( __( 'Hello Dolly Lyrics' ), __( 'Hello Dolly Lyrics' ), 'manage_options', 'hd2_options', array( &$this, 'options' ) );
+	}
+
+
+	/**
+	 * Callback to load options template
+	 */
+	function options() {
+		$this->template->load( 'sample-options');
+	}
+
+
+	/**
+	 * Sanitizes options on save
+	 * @param unknown $options
+	 * @return unknown
+	 */
+	function options_validate( $options ) {
+
+		if ( !is_array( $options['lyrics'] ) ) {
+			$options['lyrics'] = explode( "\n", $options['lyrics'] );
+		}
+
+		foreach ( $options['lyrics'] as $ID => &$lyric )
+			$lyric = trim( stripslashes( wp_filter_nohtml_kses( $lyric ) ) );
+
+		return $options;
+
+	}
+
+
 }
+
 
 $hd2 = new Hello_Dolly2();
